02920d4d22d48ece283a4dfa35ac7245b1684765
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-13 19:26:52 -0400

    Add user model and database configuration to app.py
    - Define User model with fields for id, first_name, last_name,
      username, and email.
    - Configure SQLAlchemy with database URI and disable track
      modifications.
    - Initialize database within app context.

--- project_2/app.py
+++ project_2/app.py
@@ -1,6 +1,23 @@
+from constants import DB_URI
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+# init Flask
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
+app.config["SQLALCHEMY_TACK_MODIFICATIONS"] = False
+
+
+db = SQLAlchemy(app)
+
+
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    username = db.Column(db.String(8), nullable=False, unique=True)
+    email = db.Column(db.String(25), nullable=False, unique=True)
 
 
 @app.route("/")
@@ -9,4 +26,6 @@ def hello():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
