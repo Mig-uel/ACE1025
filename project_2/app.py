7dd9c5bd886aaed812617ec7b6e4b4264f5737c4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-15 21:24:18 -0400

    Rename variable for clarity in home function - Changed 'boxes' to 'products' for better readability - Improves understanding of the data being passed to the template

--- project_2/app.py
+++ project_2/app.py
@@ -78,9 +78,9 @@ class Category(db.Model):
 def home():
     year = datetime.now().year
     query = select(Product).limit(limit=4)
-    boxes = db.session.scalars(query).all()
+    products = db.session.scalars(query).all()
 
-    return render_template("index.html", products=boxes, year=year)
+    return render_template("index.html", products=products, year=year)
 
 
 @app.route("/register", methods=["GET", "POST"])
